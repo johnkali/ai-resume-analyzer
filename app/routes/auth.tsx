9e79cd9b837a9b1392010bec336f626bf2f062d8
johnkali <jdkali96@gmail.com>
2025-11-20 16:43:30 +0300

    Add some basid Auth text on page

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 
 export  const meta = () => ([
     {title: 'ResumAI | Auth'},
@@ -7,10 +7,18 @@ export  const meta = () => ([
 ])
 
 const Auth = () => {
+
     return (
-        <div>
-            Auth
-        </div>
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Welcome</h1>
+                        <h2>Log In to Continue Your Job Journey</h2>
+                    </div>
+                </section>
+            </div>
+        </main>
     );
 };
 
