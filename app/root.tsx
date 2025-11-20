39ebbeb2859f9e7f77a350a16678425dc7b9b212
johnkali <jdkali96@gmail.com>
2025-11-20 16:35:45 +0300

    Add Puter configurationsand type declarations

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
+
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
@@ -33,7 +34,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+      <script src="https://js.puter.com/v2/"></script>
+
+      {children}
         <ScrollRestoration />
         <Scripts />
       </body>
---
+++ app/routes/auth.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+export  const meta = () => ([
+    {title: 'ResumAI | Auth'},
+    {name: 'description', content: 'Log into your account'},
+])
+
+const Auth = () => {
+    return (
+        <div>
+            Auth
+        </div>
+    );
+};
+
+export default Auth;
