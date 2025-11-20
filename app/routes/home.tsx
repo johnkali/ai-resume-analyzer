36a9b4ea3b5be8400449617441fda2209070135a
johnkali <jdkali96@gmail.com>
2025-11-20 15:49:36 +0300

    Create and render resume card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import {resumes} from '../../constants';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -18,6 +20,16 @@ export default function Home() {
       </div>
     </section>
 
+    {resumes.length > 0 && (
 
+    <div className="resumes-section">
+       {resumes.map((resume) => (
+
+       <ResumeCard key={resume.id} resume={resume} />
+
+      ))}      
+    </div>
+
+)}
   </main>;
 }
