528d663a240f11a7370dfbcd39ebb3ee97e6ec4f
johnkali <jdkali96@gmail.com>
2025-11-21 15:10:58 +0300

    Set up Puter authentication in application

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
 
 
 export const links: Route.LinksFunction = () => [
@@ -25,6 +27,13 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+    const {init} = usePuterStore();
+
+    useEffect(() => {
+      init();
+    },[init])
+
+
   return (
     <html lang="en">
       <head>
--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,5 +1,7 @@
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
 
 export  const meta = () => ([
     {title: 'ResumAI | Auth'},
@@ -7,6 +9,15 @@ export  const meta = () => ([
 ])
 
 const Auth = () => {
+    const {isLoading, auth} = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    //if user is authenticated,redirect user to next page
+    useEffect( ()=> {
+        if(auth.isAuthenticated) navigate(next)
+    }, [auth.isAuthenticated, next])
 
     return (
         <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
@@ -15,6 +26,25 @@ const Auth = () => {
                     <div className="flex flex-col items-center gap-2 text-center">
                         <h1>Welcome</h1>
                         <h2>Log In to Continue Your Job Journey</h2>
+                    </div>
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                <p>Signing you in...</p>
+                            </button>
+                        ):(
+                            <>
+                                {auth.isAuthenticated ? (
+                                    <button className="auth-button" onClick={auth.signOut}>
+                                        <p>Log Out</p>
+                                    </button>
+                                ): (
+                                    <button className="auth-button" onClick={auth.signIn}>
+                                        <p>Log In</p>
+                                    </button>
+                                )}
+                            </>
+                        )}
                     </div>
                 </section>
             </div>
