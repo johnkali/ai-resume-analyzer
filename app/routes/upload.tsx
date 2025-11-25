00a02d731e685a375fc186f2d1902e850942b0b0
johnkali <jdkali96@gmail.com>
2025-11-25 15:29:40 +0300

    Finish PDF upload, kv storage and AI feedback

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -5,6 +5,7 @@ import {usePuterStore} from "~/lib/puter";
 import {useNavigate} from "react-router";
 import {convertPdfToImage} from "~/lib/pdf2img";
 import {generateUUID} from "~/lib/utils";
+import {prepareInstructions} from "../../constants";
 
 const Upload = () => {
     const {auth, isLoading, fs, ai, kv} = usePuterStore();
@@ -22,12 +23,11 @@ const Upload = () => {
     setIsProcessing(true);
     setStatusText('Uploading the file...');
     const uploadedFile = await fs.upload([file]);
-
     if(!uploadedFile) return setStatusText('Error: Failed to upload file');
 
-    setStatusText('Converting to image...');
-    const imageFile =  await convertPdfToImage(file);
-    if(!imageFile.file) return setStatusText('Error: Failed to convert PDF to image');
+        setStatusText('Converting to image...');
+        const imageFile = await convertPdfToImage(file);
+        if(!imageFile.file) return setStatusText('Error: Failed to convert PDF to image');
 
     setStatusText('Uploading the image...');
     const uploadedImage  = await fs.upload([imageFile.file]);
@@ -40,8 +40,27 @@ const Upload = () => {
         id: uuid,
         resumePath: uploadedFile.path,
         companyName, jobTitle, jobDescription, file,
-        feedback: ''
+        feedback: '',
     }
+    await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+    setStatusText('Analyzing ...');
+
+    const feedback =  await ai.feedback(
+        uploadedFile.path,
+        prepareInstructions({ jobTitle, jobDescription })
+    );
+
+    if(!feedback) return setStatusText('Error: Failed to analyze resume');
+
+    const feedbackText =  typeof feedback.message.content === 'string'
+        ? feedback.message.content
+        : feedback.message.content[0].text;
+
+    data.feedback = JSON.parse(feedbackText);
+    await kv.set(`resume:${uuid}`, JSON.stringify(data));
+    setStatusText('Analysis complete, redirecting ...');
+    console.log(data);
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>)=>{
