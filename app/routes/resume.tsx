c6fecc468c94fd84bd104d3a1bc8b6f22d425d19
johnkali <jdkali96@gmail.com>
2025-12-08 11:37:15 +0300

    Implement resume feedback page

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {Link, useNavigate, useParams} from "react-router";
 import {usePuterStore} from "~/lib/puter";
+import Summary from "~/components/Summary";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
 
 export  const meta = () => ([
     {title: 'ResumAI | Review'},
@@ -61,6 +64,18 @@ const Resume = () => {
                                 />
                             </a>
                         </div>
+                    )}
+                </section>
+                <section className="feedback-section">
+                    <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
+                    {feedback ? (
+                        <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+                            <Summary  feedback={feedback}/>
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips}/>
+                            <Details feedback={feedback}/>
+                        </div>
+                    ) : (
+                        <img src="/images/resume-scan-2.gif" alt="resume gif"/>
                     )}
                 </section>
             </div>
